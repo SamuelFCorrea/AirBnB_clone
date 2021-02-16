28cff9d033151a0d76d2f3c8aa7ba6e7ec5c0412
Samuel Correa <samuelfelipecorrea@gmail.com>
2021-02-16 17:52:07 -0500

    Tab

--- console.py
+++ console.py
@@ -232,7 +232,9 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == '__main__':
+    
     import sys
+    
     if len(sys.argv) >= 2:
         HBNBCommand().onecmd(' '.join(sys.argv[1:]))
     else:
