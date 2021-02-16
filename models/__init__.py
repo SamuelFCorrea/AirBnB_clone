796ba43bfa0d7e6f9ff309c3d90dbfefb0315614
Samuel Correa <samuelfelipecorrea@gmail.com>
2021-02-15 21:56:18 -0500

    Beautiful doc

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,33 @@
+'''
+    ______   _______   ______    _____    _    _   _____
+   |  __  | |___ ___| |  __  |  |  _  |  | \  | | |  _  |
+   | /__\ |    | |    | |__|  | | |_|  | |  \ | | | |_|  |
+   |  __  |    | |    |  __  |  |  _  -  |   \| | |  _  -
+   | |  | |  __| |__  | |  \ \  | |_|  | | |\   | | |_|  |  _
+   |_|  |_| |_______| |_|   \_\ |_____|  |_| \__| |_____|  |_|
+
+
+All the classes needed to the AirBnB clone:
+
+    BaseModel: assing id and the updated/created time parentclass for the other ones
+    User: Public class attributes 'email', 'password', 'first_name', 'last_name'
+    State: Public class attributes 'name'
+    City: Public class attributes 'state_id', 'name'
+    Amenity: Public class attributes 'name'
+    Place: Public class attributes 'city_id', 'user_id', 'name', 'description',
+           'number_rooms', 'number_bathrooms', 'max_guest', 'price_by_night',
+           'latitude', 'longitude', 'amenity_ids'
+    Review: Public class attributes 'place_id', 'user_id', 'text'
+
+    One class per file.
+
+    BaseModel methods:
+        save(): save the object and actualice the 'updated_at' date
+        to_dict(): return a dictionary with all the object atributtes and the class name
+
+    String representation model: [<class name>] (<self.id>) <self.__dict__>
+    '''
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
