05f73b919dd6ccc7b8a4b8bafc2a86dad1f6cff2
Samuel Correa <samuelfelipecorrea@gmail.com>
2021-02-16 18:00:53 -0500

    Removed argv input option

--- console.py
+++ console.py
@@ -232,10 +232,5 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    
     import sys
-    
-    if len(sys.argv) >= 2:
-        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
-    else:
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
